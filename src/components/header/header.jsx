deb721693a3818db1a917cfaf78f041db0dca527
mjkim <91gem@naver.com>
2022-05-20 19:03:42 +0900

    navLink에서 link로 수정

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import BtnAddPost from '../buttons/btn-add-post';
 import BtnHambuger from '../buttons/btn-hambuger';
 import BtnMessage from '../buttons/btn-message';
@@ -7,9 +7,9 @@ import BtnSearch from '../buttons/btn-search';
 import styles from './header.module.css'
 
 const Header = (props) => {
-    const navigator = useNavigate();
+    const navigate = useNavigate();
     const goToMain = () => {
-            navigator(
+        navigate(
             {pathname: '/'},
         )
     }
@@ -17,7 +17,10 @@ const Header = (props) => {
 
     return(
         <header>
-            <h1 className={styles.headLogo} onClick={goToMain}><img src='./images/insta-font-logo.png' alt='logo' /></h1>
+             <Link to='/'>
+                 <h1 className={styles.headLogo} onClick={goToMain}><img src='./images/insta-font-logo.png' alt='logo' /></h1>
+             </Link>
+            
             <div className={styles.btnArea}>
                 <BtnAddPost />
                 <BtnSearch />
